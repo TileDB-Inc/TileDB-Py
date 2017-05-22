9f9ba16564500a878a72fdcc73205b5e408ec7ae
Jake Bolewski <jakebolewski@gmail.com>
2017-05-22 18:23:58 -0400

    Update __init__.py

--- tiledb/__init__.py
+++ tiledb/__init__.py
@@ -4,6 +4,6 @@ def _version():
     libtiledb = ctypes.cdll.LoadLibrary("libtiledb.dylib")
     major, minor, rev = ctypes.c_int(0), ctypes.c_int(0), ctypes.c_int(0)
     libtiledb.tiledb_version(ctypes.byref(major), ctypes.byref(minor), ctypes.byref(rev))
-    return (int(major), int(minor), int(rev))
+    return (major.value, minor.value, rev.value)
 
 __version__ = "{:d}.{:d}.{:d}".format(*_version())
