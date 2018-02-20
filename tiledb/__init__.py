2c8ba9f6103dc43793b113cf056f93c497c8355a
Jake Bolewski <jakebolewski@gmail.com>
2018-02-20 14:23:18 -0700

    add libtiledb version check

--- tiledb/__init__.py
+++ tiledb/__init__.py
@@ -4,3 +4,7 @@ from .libtiledb import (Ctx, Config, Dim, Domain, Attr, DenseArray, SparseArray,
 
 __all__ = [Ctx, Config, Dim, Domain, Attr, DenseArray, SparseArray, TileDBError, VFS,
            group_create, ls, walk, remove, move]
+
+if libtiledb.version() != (1, 2, 0):
+    raise RuntimeError("The libtiledb library version does not match "
+                       "the latest released version of tiledb v.1.2.0")
