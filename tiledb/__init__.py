20704b16cf3aa43e8f0e29e3b47c7b96bccf7c4a
Jake Bolewski <jakebolewski@gmail.com>
2018-02-20 15:54:56 -0700

    add array consolidate to the exports list

--- tiledb/__init__.py
+++ tiledb/__init__.py
@@ -1,10 +1,10 @@
 from __future__ import absolute_import
 
 from .libtiledb import (Ctx, Config, Dim, Domain, Attr, KV, DenseArray, SparseArray, TileDBError, VFS,
-                        group_create, object_type, ls, walk, remove, move)
+                        array_consolidate, group_create, object_type, ls, walk, remove, move)
 
 __all__ = [Ctx, Config, Dim, Domain, Attr, KV, DenseArray, SparseArray, TileDBError, VFS,
-           group_create, object_type, ls, walk, remove, move]
+           array_consolidate, group_create, object_type, ls, walk, remove, move]
 
 if libtiledb.version() != (1, 2, 0):
     raise RuntimeError("The libtiledb library version does not match "
