6849ea31689cb479f68dfd29a6e19b3931017be4
Jake Bolewski <jakebolewski@gmail.com>
2018-03-06 13:54:53 -0700

    add env variable to turn off version checking

--- tiledb/__init__.py
+++ tiledb/__init__.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
+import os
 
 from .libtiledb import (Ctx, Config, Dim, Domain, Attr, KV, ArraySchema, DenseArray, SparseArray,
                         TileDBError, VFS, array_consolidate, group_create, object_type,
@@ -7,6 +8,7 @@ from .libtiledb import (Ctx, Config, Dim, Domain, Attr, KV, ArraySchema, DenseAr
 __all__ = [Ctx, Config, Dim, Domain, Attr, KV, ArraySchema, SparseArray, TileDBError, VFS,
            array_consolidate, group_create, object_type, ls, walk, remove, move]
 
-if libtiledb.version() != (1, 2, 0):
-    raise RuntimeError("The libtiledb library version does not match "
-                       "the latest released version of tiledb v.1.2.0")
+if not os.environ.get("TILEDB_PY_NO_VERSION_CHECK"):
+    if libtiledb.version() != (1, 2, 0):
+        raise RuntimeError("The libtiledb library version does not match "
+                           "the latest released version of tiledb v.1.2.0")
