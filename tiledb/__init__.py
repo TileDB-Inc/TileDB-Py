849f0c744fd558dc5bdeac89d081c06d4464df7e
Jake Bolewski <jakebolewski@gmail.com>
2018-03-14 11:03:05 -0600

    remove version check

--- tiledb/__init__.py
+++ tiledb/__init__.py
@@ -1,5 +1,4 @@
 from __future__ import absolute_import
-import os
 
 from .libtiledb import (Ctx, Config, Dim, Domain, Attr, KV, ArraySchema, DenseArray, SparseArray,
                         TileDBError, VFS, array_consolidate, group_create, object_type,
@@ -7,9 +6,3 @@ from .libtiledb import (Ctx, Config, Dim, Domain, Attr, KV, ArraySchema, DenseAr
 
 __all__ = [Ctx, Config, Dim, Domain, Attr, KV, ArraySchema, SparseArray, TileDBError, VFS,
            array_consolidate, group_create, object_type, ls, walk, remove, move]
-
-if not os.environ.get("TILEDB_PY_NO_VERSION_CHECK"):
-    major, minor, patch = libtiledb.version()
-    if (major, minor) > (1, 2):
-        raise RuntimeError("The libtiledb library version does not match "
-                           "the latest released version of tiledb 1.2.x")
