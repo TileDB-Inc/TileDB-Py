c901e89f1750649c3a74119c07c69b86a723c49b
Tyler Denniston <tyler@tiledb.io>
2018-07-09 16:42:05 -0400

    Sync sidebar; bump docs version num; small docstring fixes.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -25,9 +25,9 @@ copyright = '2018, TileDB, Inc.'
 author = 'TileDB, Inc.'
 
 # The short X.Y version
-version = '0.1'
+version = '0.2'
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
