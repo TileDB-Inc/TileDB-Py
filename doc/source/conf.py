faee2fa05fdb1f8f5ea2ba7b1b4e1f818e3df8d9
Jake Bolewski <jakebolewski@gmail.com>
2018-10-26 10:13:54 -0600

    Update docs to 0.3.0, fix TileDB version to 1.4.0

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -25,9 +25,9 @@ copyright = '2018, TileDB, Inc.'
 author = 'TileDB, Inc.'
 
 # The short X.Y version
-version = '0.2'
+version = '0.3'
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.3.0'
 
 
 # -- General configuration ---------------------------------------------------
