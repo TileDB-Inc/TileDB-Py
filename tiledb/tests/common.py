8ab2cad2554fe3279d111a28bd512f498c55dd19
Isaiah Norton <isaiah@tiledb.io>
2019-05-17 11:31:40 -0400

    Fixes for CI build
    - Add psutil install on azure
    - Fix file deletion on windows

--- tiledb/tests/common.py
+++ tiledb/tests/common.py
@@ -4,9 +4,11 @@ import glob
 import os
 import shutil
 import tempfile
+import traceback
 from unittest import TestCase
 
 class DiskTestCase(TestCase):
+    pathmap = dict()
 
     def setUp(self):
         prefix = 'tiledb-' + self.__class__.__name__
@@ -20,8 +22,14 @@ class DiskTestCase(TestCase):
             except OSError as exc:
                 print("test '{}' error deleting '{}'".format(self.__class__.__name__,
                                                              dirpath))
-                raise
+                print("registered paths and originating functions:")
+                for path,frame in self.pathmap.items():
+                    print("  '{}' <- '{}'".format(path,frame))
+                raise exc
 
     def path(self, path):
-        return os.path.abspath(os.path.join(self.rootdir, path))
+        out = os.path.abspath(os.path.join(self.rootdir, path))
+        frame = traceback.extract_stack(limit=2)[-2][2]
+        self.pathmap[out] = frame
+        return out
 
