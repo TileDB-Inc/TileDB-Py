dcd77b3a3307f29b100d566cec5805a735e18d1e
Isaiah Norton <isaiah@tiledb.io>
2019-07-25 16:13:01 -0400

    Modular build example (to revert)

--- tiledb/np2buf.pyx
+++ tiledb/np2buf.pyx
@@ -1,5 +1,5 @@
 # Set true to enable modular compilation
-DEF TILEDBPY_MODULAR = False
+DEF TILEDBPY_MODULAR = True
 IF TILEDBPY_MODULAR:
     include "common.pxi"
     from cpython.version cimport PY_MAJOR_VERSION
