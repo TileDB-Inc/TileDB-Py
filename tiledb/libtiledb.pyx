6538301ed76d8bab6df97707055e1a3747d2e8a4
Isaiah Norton <isaiah@tiledb.io>
2019-07-25 14:43:27 -0400

    Modularization of varlen output buffer functions
    This change allows compiling conversion functions to a separate
    extension during development, in order to avoid recompiling  100k LOC
    cython C++ output for every change.
    
    This is disabled by default in order to avoid distribution complication.

--- tiledb/libtiledb.pyx
+++ tiledb/libtiledb.pyx
@@ -2,61 +2,50 @@
 #cython: embedsignature=True
 #cython: auto_pickle=False
 
+include "common.pxi"
 
 from cpython.version cimport PY_MAJOR_VERSION
-
-from cpython.bytes cimport (PyBytes_GET_SIZE,
-                            PyBytes_AS_STRING,
-                            PyBytes_Size,
-                            PyBytes_FromString,
-                            PyBytes_FromStringAndSize)
-
-
-from cpython.ref cimport (Py_INCREF, Py_DECREF, PyTypeObject)
-
-from libc.stdio cimport (FILE, stdout)
-from libc.stdio cimport stdout
-from libc.stdlib cimport malloc, calloc, free
-from libc.string cimport memcpy
-from libc.stdint cimport (uint8_t, uint64_t, int64_t, uintptr_t)
-from libc cimport limits
-from libcpp.vector cimport vector
-
-cdef extern from "Python.h":
-    object PyUnicode_FromStringAndSize(const char *u, Py_ssize_t size)
-
-# Numpy imports
-"""
-cdef extern from "numpyFlags.h":
-    # Include 'numpyFlags.h' into the generated C code to disable warning.
-    # This must be included before numpy is cimported
-    pass
-"""
-
-import numpy as np
-cimport numpy as np
-cimport cython
-
-cdef extern from "numpy/arrayobject.h":
-    # Steals a reference to dtype, need to incref the dtype
-    object PyArray_NewFromDescr(PyTypeObject* subtype,
-                                np.dtype descr,
-                                int nd,
-                                np.npy_intp* dims,
-                                np.npy_intp* strides,
-                                void* data,
-                                int flags,
-                                object obj)
-    # Steals a reference to dtype, need to incref the dtype
-    object PyArray_Scalar(void* ptr, np.dtype descr, object itemsize)
-    void PyArray_ENABLEFLAGS(np.ndarray arr, int flags)
-    void* PyDataMem_NEW(size_t nbytes)
-    void* PyDataMem_RENEW(void* data, size_t nbytes)
-    void PyDataMem_FREE(void* data)
 
 import sys
 from os.path import abspath
 from collections import OrderedDict
+
+###############################################################################
+#     Numpy initialization code (critical)                                    #
+###############################################################################
+
+# https://docs.scipy.org/doc/numpy/reference/c-api.array.html#c.import_array
+np.import_array()
+
+###############################################################################
+#     Global Ctx object                                                       #
+###############################################################################
+# Ctx used by default in all constructors
+# Users needing a specific context should pass their own context as kwarg.
+cdef Ctx _global_ctx = Ctx()
+
+def default_ctx():
+    """Returns the default tiledb.Ctx object"""
+    return _global_ctx
+
+###############################################################################
+#    MODULAR IMPORTS                                                          #
+###############################################################################
+
+# Set true to enable modular compilation.
+# This variable must be updated in all pyx files!
+DEF TILEDBPY_MODULAR = False
+
+# np2buf.pyx
+IF TILEDBPY_MODULAR:
+    from np2buf import array_to_buffer
+ELSE:
+    include "np2buf.pyx"
+
+
+###############################################################################
+#    Utility/setup                                                            #
+###############################################################################
 
 # Integer types supported by Python / System
 if sys.version_info >= (3, 0):
@@ -72,10 +61,6 @@ _MB = 1024 * _KB
 
 # The native int type for this platform
 IntType = np.dtype(np.int_)
-
-# Numpy initialization code (critical)
-# https://docs.scipy.org/doc/numpy/reference/c-api.array.html#c.import_array
-np.import_array()
 
 def version():
     """Return the version of the linked ``libtiledb`` shared library
@@ -94,16 +79,6 @@ def version():
 def offset_size():
     """Return the offset size (TILEDB_OFFSET_SIZE)"""
     return tiledb_offset_size()
-
-
-# Global Ctx object to be used by default in all constructors
-# Users needing a specific context should pass a context as kwarg.
-cdef Ctx _global_ctx = Ctx()
-
-def default_ctx():
-    """Returns the default tiledb.Ctx object"""
-    return _global_ctx
-
 
 def regularize_tiling(tile, ndim):
     if not tile:
@@ -295,6 +270,12 @@ cdef bytes unicode_path(object path):
     """Returns a UTF-8 encoded byte representation of a given URI path string"""
     return ustring(path).encode('UTF-8')
 
+
+###############################################################################
+#                                                                             #
+#    CLASS DEFINITIONS                                                        #
+#                                                                             #
+###############################################################################
 
 cdef class Config(object):
     """TileDB Config class
@@ -3652,139 +3633,6 @@ cdef class Array(object):
                  array.dtype == np.object
                  )
 
-    cdef _varlen_dtype_itemsize(self, object item):
-        if (isinstance(item, np.dtype) and np.issubdtype(item, np.bytes_)):
-            return sizeof(char)
-        elif isinstance(item, np.dtype):
-            return item.itemsize
-        elif item == np.bytes_:
-            return sizeof(char)
-        elif item == np.unicode_:
-            # Note this is just a place-holder, we call CPython API to get actual size
-            return sizeof(char)
-
-        raise TypeError("Unknown dtype itemsize for '{}'.".format(item))
-
-    cdef _varlen_type_compat(self, object reftype, object val):
-        if isinstance(val, bytes) and reftype is bytes:
-            return True
-        elif (val, np.bytes_) and np.issubdtype(reftype, np.bytes_):
-            return True
-        elif isinstance(val, np.unicode_) and np.issubdtype(reftype, np.unicode_):
-            return True
-        elif isinstance(val, np.ndarray):
-            if reftype == val.dtype:
-                return True
-            elif np.issubdtype(val.dtype, np.bytes_) and np.issubdtype(reftype, np.bytes_):
-                return True
-            elif np.issubdtype(val.dtype, np.string_) and np.issubdtype(reftype, np.string_):
-                return True
-        # default case
-        return False
-
-    cdef _varlen_cell_dtype(self, object var):
-        cdef np.dtype _dtype
-        if isinstance(var, np.ndarray):
-            _dtype = var.dtype
-            if np.issubdtype(_dtype, np.bytes_):
-                # handles 'S[n]' dtypes for all n
-                return np.bytes_
-            elif np.issubdtype(_dtype, np.unicode_):
-                # handles 'U[n]' dtypes for all n
-                return np.unicode_
-            else:
-                return _dtype
-        elif isinstance(var, bytes):
-            return np.bytes_
-        elif isinstance(var, unicode):
-            return np.unicode_
-
-        raise TypeError("Unsupported varlen cell datatype")
-
-    cdef _pack_varlen_bytes(self, object val):
-        cdef arr = <np.ndarray?>val
-
-        if len(arr) == 0:
-            raise Exception("Empty arrays are not supported.")
-
-        assert((arr.dtype == np.dtype('O') or
-                np.issubdtype(arr.dtype, np.bytes_) or
-                np.issubdtype(arr.dtype, np.unicode_)),
-               "_pack_varlen_bytes: input array must be np.object or np.bytes!")
-
-
-        first_dtype = self._varlen_cell_dtype(arr[0])
-        # item size
-        cdef uint64_t el_size = self._varlen_dtype_itemsize(first_dtype)
-
-        if el_size==0:
-            raise TypeError("Zero-size cell elements are not supported.")
-
-        # total buffer size
-        cdef uint64_t buffer_size = 0
-        cdef np.ndarray buffer_offsets = np.empty(len(arr), dtype=np.uint64)
-        cdef uint64_t el_buffer_size = 0
-
-        # first pass: check types and calculate offsets
-        for (i, item) in enumerate(arr):
-            if first_dtype != self._varlen_cell_dtype(item):
-                msg = ("Data types of variable-length sub-arrays must be consistent. "
-                       "Type '{}', of 1st sub-array, is inconsistent with type '{}', of item {}."
-                       ).format(first_dtype, self._varlen_cell_dtype(item), i)
-
-                raise TypeError(msg)
-
-            # current offset is last buffer_size
-            buffer_offsets[i] = buffer_size
-
-            if first_dtype == np.unicode_:
-                # this will cache the materialized (if any) UTF8 object
-                if PY_MAJOR_VERSION >= 3:
-                    utf8 = (<str>item).encode('UTF-8')
-                else:
-                    utf8 = (<unicode>item).encode('UTF-8')
-                el_buffer_size = len(utf8)
-            else:
-                el_buffer_size = el_size * len(item)
-            assert(el_buffer_size > 0)
-
-            # *running total* buffer size
-            buffer_size += el_buffer_size
-
-        # return a numpy buffer because that is what the caller uses for non-varlen buffers
-        cdef np.ndarray buffer = np.zeros(shape=buffer_size, dtype=np.uint8)
-        # <TODO> should be np.empty(shape=buffer_size, dtype=np.uint8)
-        cdef char* buffer_ptr = <char*>np.PyArray_DATA(buffer)
-        cdef char* input_ptr = NULL
-        cdef object tmp_utf8 = None
-
-        # bytes to copy in this block
-        cdef uint64_t nbytes = 0
-        # loop over sub-items and copy into buffer
-        for (i, subarray) in enumerate(val):
-            if (isinstance(subarray, bytes) or
-                    (isinstance(subarray, np.ndarray) and np.issubdtype(subarray.dtype, np.bytes_))):
-                input_ptr = <char*>PyBytes_AS_STRING(subarray)
-            elif (isinstance(subarray, str) or (isinstance(subarray, unicode)) or
-                  (isinstance(subarray, np.ndarray) and np.issubdtype(subarray.dtype, np.unicode_))):
-                tmp_utf8 = subarray.encode("UTF-8")
-                input_ptr = <char*>tmp_utf8
-            else:
-                input_ptr = <char*>np.PyArray_DATA(subarray)
-
-            if i == len(val)-1:
-                nbytes = buffer_size - buffer_offsets[i]
-            else:
-                nbytes = buffer_offsets[i+1] - buffer_offsets[i]
-
-            memcpy(buffer_ptr, input_ptr, nbytes)
-            buffer_ptr += nbytes
-            # clean up the encoded object *after* storing
-            if tmp_utf8:
-                del tmp_utf8
-
-        return buffer, buffer_offsets
-
     @cython.boundscheck(False)
     cdef _unpack_varlen_query(self, ReadQuery read, unicode name):
         assert(name != "coords")
@@ -4438,7 +4286,7 @@ cdef class DenseArray(Array):
         output_offsets = list()
         for i in range(nattr):
             if self._ndarray_is_varlen(values[i]):
-                buffer, offsets = self._pack_varlen_bytes(values[i])
+                buffer, offsets = array_to_buffer(values[i])
                 buffer_sizes[i] = buffer.nbytes
                 buffer_offsets_sizes[i] = offsets.nbytes
                 output_values.append(buffer)
@@ -4762,7 +4610,7 @@ cdef class SparseArray(Array):
             for i, (name, buffer) in enumerate(values.items()):
                 battr_name = name.encode('UTF-8')
                 if self._ndarray_is_varlen(buffer):
-                    packed_buffer, offsets = self._pack_varlen_bytes(buffer)
+                    packed_buffer, offsets = array_to_buffer(buffer)
                     buffer_sizes[i] = packed_buffer.nbytes
                     offsets_size = offsets.nbytes
                     buffer_ptr = np.PyArray_DATA(packed_buffer)
