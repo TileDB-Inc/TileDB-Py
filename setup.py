5d3e414de8f38eb0c040941c3c8331f68f05c222
Isaiah Norton <isaiah@tiledb.io>
2020-01-20 10:43:42 -0500

    TileDB-Py release branch 0.5.5 against libtiledb 1.7.4

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ import sys
 from sys import version_info as ver
 
 # Target branch
-TILEDB_VERSION = "dev"
+TILEDB_VERSION = "1.7.4"
 # allow overriding w/ environment variable
 TILEDB_VERSION = os.environ.get("TILEDB_VERSION") or TILEDB_VERSION
 
