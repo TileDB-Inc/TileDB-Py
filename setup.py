e91f9454332b381b2cecd505f5cbbc9e9f128ad5
Isaiah Norton <isaiah@tiledb.io>
2020-05-04 18:55:26 -0400

    Bump release CI versions

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ import sys
 from sys import version_info as ver
 
 # Target branch
-TILEDB_VERSION = "dev"
+TILEDB_VERSION = "2.0.0"
 # allow overriding w/ environment variable
 TILEDB_VERSION = os.environ.get("TILEDB_VERSION") or TILEDB_VERSION
 
