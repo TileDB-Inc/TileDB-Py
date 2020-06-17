a53e379b6b74af6acbffe95d4d44fc241bb8dcdc
Isaiah Norton <isaiah@tiledb.io>
2020-06-17 17:06:39 -0400

    Update HISTORY.md for TileDB-Py 0.6.3 / TileDB 2.0.5

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -27,7 +27,7 @@ author = 'TileDB, Inc.'
 # The short X.Y version
 version = '0.6'
 # The full version, including alpha/beta/rc tags
-release = '0.6.2'
+release = '0.6.3'
 
 
 # -- General configuration ---------------------------------------------------
