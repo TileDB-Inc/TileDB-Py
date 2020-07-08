b2c795807b54bfe828b5d10144b450699c231b70
Isaiah Norton <isaiah@tiledb.io>
2020-07-08 15:12:00 -0400

    Bump version to 0.6.4

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -27,7 +27,7 @@ author = 'TileDB, Inc.'
 # The short X.Y version
 version = '0.6'
 # The full version, including alpha/beta/rc tags
-release = '0.6.3'
+release = '0.6.4'
 
 
 # -- General configuration ---------------------------------------------------
