6c60b79fd603b1f15adefd089848651b36efb1e2
Isaiah Norton <isaiah@tiledb.io>
2021-01-04 22:13:18 -0500

    Revert "Addition of FragmentInfo Python API (#426)"
    This reverts commit 4bb4731e38fb34feaa93c1354430d5b4fefc6443.

--- tiledb/core.cc
+++ tiledb/core.cc
@@ -36,6 +36,24 @@ using namespace tiledb;
 namespace py = pybind11;
 using namespace pybind11::literals;
 
+#define TPY_ERROR_LOC(m)                                                       \
+  throw TileDBPyError(std::string(m) + " (" + __FILE__ + ":" +                 \
+                      std::to_string(__LINE__) + ")");
+
+const uint64_t DEFAULT_INIT_BUFFER_BYTES = 1310720 * 8;
+const uint64_t DEFAULT_EXP_ALLOC_MAX_BYTES = uint64_t(4 * pow(2, 30));
+
+class TileDBPyError : std::runtime_error {
+public:
+  explicit TileDBPyError(const char *m) : std::runtime_error(m) {}
+  explicit TileDBPyError(std::string m) : std::runtime_error(m.c_str()) {}
+
+public:
+  virtual const char *what() const noexcept override {
+    return std::runtime_error::what();
+  }
+};
+
 using TimerType = std::chrono::duration<double>;
 struct StatsInfo {
   std::map<std::string, TimerType> counters;
