5e0dbf58dedcf6a50118108b89de31d5c539650f
Isaiah Norton <isaiah@tiledb.io>
2021-01-23 23:21:25 -0500

    Fix #457: make sure to fit automatic tile extent to dim range for date type

--- tiledb/dataframe_.py
+++ tiledb/dataframe_.py
@@ -257,7 +257,7 @@ def dim_for_column(ctx, name, dim_info, col, tile=None, full_domain=False,
         dim_max = np.max(col_values)
 
     if not dim_info.dtype in (np.bytes_, np.unicode):
-        if np.issubdtype(dtype, np.integer):
+        if np.issubdtype(dtype, np.integer) or dtype.kind == 'M':
             dim_range = np.uint64(np.abs(np.uint64(dim_max) - np.uint64(dim_min)))
             # we can't make a tile larger than the dimension range
             if dim_range < tile:
