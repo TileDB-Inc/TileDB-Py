0428256f418e8775097aae7ee933679f2c5fcf6e
Vivian Nguyen <vivian@tiledb.com>
2021-02-26 20:11:28 -0600

    Clang-Format C Header Files

--- tiledb/npbuffer.h
+++ tiledb/npbuffer.h
@@ -1,13 +1,14 @@
-#include <pybind11/pybind11.h>
-#include <pybind11/numpy.h>
-#include <pybind11/pytypes.h>
 #include "util.h"
+#include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/pytypes.h>
 
 namespace tiledbpy {
 
 namespace py = pybind11;
 using namespace pybind11::literals;
 
-py::tuple convert_np(py::array input, bool allow_unicode = true, bool use_fallback = false);
+py::tuple convert_np(py::array input, bool allow_unicode = true,
+                     bool use_fallback = false);
 
-}
+} // namespace tiledbpy
