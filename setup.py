ca6e04f5b52a4c6e83454239c635f2b2acffcf90
Vivian Nguyen <vivian@tiledb.com>
2021-02-26 20:03:47 -0600

    Format Python Files With Latest Stable Version of Black

--- setup.py
+++ setup.py
@@ -488,9 +488,9 @@ class LazyCommandClass(dict):
 
 class get_pybind_include(object):
     """Helper class to determine the pybind11 include path
-  The purpose of this class is to postpone importing pybind11
-  until it is actually installed, so that the ``get_include()``
-  method can be invoked. """
+    The purpose of this class is to postpone importing pybind11
+    until it is actually installed, so that the ``get_include()``
+    method can be invoked."""
 
     def __init__(self, user=False):
         self.user = user
