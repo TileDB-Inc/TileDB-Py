47c03c7311a9118f149ed36fdaebae462d20b153
Isaiah Norton <isaiah@tiledb.io>
2021-04-29 21:42:58 -0400

    Bump to 0.8.8 / libtiledb 2.2.8

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import Extension, find_packages, setup
 print("setup.py sys.argv is: ", sys.argv)
 
 # Target branch
-TILEDB_VERSION = "2.2.7"
+TILEDB_VERSION = "2.2.8"
 # allow overriding w/ environment variable
 TILEDB_VERSION = os.environ.get("TILEDB_VERSION") or TILEDB_VERSION
 
