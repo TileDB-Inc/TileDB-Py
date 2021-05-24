cf5c9a5fed9227113b5c2422cef29b8aed64cfe0
Isaiah Norton <isaiah@tiledb.io>
2021-05-24 15:34:27 -0400

    Add test indexing with all integer dtypes

--- tiledb/tests/fixtures.py
+++ tiledb/tests/fixtures.py
@@ -7,6 +7,8 @@ import numpy as np
 from numpy.testing import assert_array_equal
 from tiledb.tests.common import assert_subarrays_equal, rand_utf8
 
+INTEGER_DTYPES = ["u1", "u2", "u4", "u8", "i1", "i2", "i4", "i8"]
+
 
 @pytest.fixture(scope="module", params=["hilbert", "row-major"])
 def sparse_cell_order(request):
