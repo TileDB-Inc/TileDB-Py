d9a72f0e6eb888d0ad793f62a0dfeed09313a18d
Isaiah Norton <isaiah@tiledb.io>
2021-06-08 16:14:21 -0400

    Revert "Relax numpy requirements in setup.py"
    This reverts commit 4c542b7bc949963677b13f3fe9a8057a85328dae.

--- setup.py
+++ setup.py
@@ -489,7 +489,8 @@ def cmake_available():
 def setup_requires():
     req = [
         "cython>=0.27",
-        "numpy>=1.16",
+        "numpy==1.16.* ; python_version < '3.9'",
+        "numpy ; python_version >= '3.9'",
         "setuptools>=18.0",
         "setuptools_scm>=1.5.4",
         "wheel>=0.30",
