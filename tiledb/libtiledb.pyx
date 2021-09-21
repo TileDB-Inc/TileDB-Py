95a18ea29d3af76a0d37a28d1baf191e475f61b0
Isaiah Norton <isaiah@tiledb.io>
2021-09-21 14:24:36 -0400

    Revert "Remove `_setitem_impl_sparse` Altogether"
    This reverts commit 611332a576c7da58575482458682418862c9ec8f.

--- tiledb/libtiledb.pyx
+++ tiledb/libtiledb.pyx
@@ -5002,6 +5002,7 @@ cdef class DenseArrayImpl(Array):
                 "Sparse writes to dense arrays is deprecated",
                 DeprecationWarning,
             )
+            _setitem_impl_sparse(self, selection, val, dict())
             return
 
         self._setitem_impl(selection, val, dict())
