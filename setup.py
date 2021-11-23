2aac633d45dd9aaf751a836eb35049235a250a2b
Isaiah Norton <isaiah@tiledb.com>
2021-11-23 15:12:18 +0000

    Add missing concurrent.futures import

--- setup.py
+++ setup.py
@@ -504,7 +504,7 @@ def setup_requires():
         req = parse_requirements("requirements_dev.txt")
         req = list(filter(lambda r: not r.startswith("-r"), req))
 
-    req_cmake = list(filter(lambda x: "cmake" in x, r))[0]
+    req_cmake = list(filter(lambda r: "cmake" in r, req))[0]
 
     # Add cmake requirement if libtiledb is not found and cmake is not available.
     if not libtiledb_exists(LIB_DIRS) and not cmake_available():
