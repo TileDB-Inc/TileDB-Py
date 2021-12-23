37496d43508d8fcafcbaa1a91afffbd04b6e40c2
Isaiah Norton <ihnorton@users.noreply.github.com>
2021-12-23 03:23:38 +0000

    Bump versions for aarch64 build

--- misc/pypi_linux/build.aarch64.sh
+++ misc/pypi_linux/build.aarch64.sh
@@ -7,10 +7,10 @@
 
 set -xeu
 
-export LIBTILEDB_VERSION=2.5.2
-export TILEDBPY_VERSION=0.11.2
+export LIBTILEDB_VERSION=2.5.3
+export TILEDBPY_VERSION=0.11.5
 
-export CIBW_MANYLINUX_AARCH64_IMAGE=wheel-host-aarch64.manylinux2014
+export CIBW_MANYLINUX_AARCH64_IMAGE=wheel-host-aarch64.manylinux2014-$LIBTILEDB_VERSION
 export CIBW_SKIP='cp27-* cp35-* cp36-* cp310-* pp-* *_i686 pp* *-musllinux*'
 export CIBW_PLATFORM='linux'
 export CIBW_ENVIRONMENT='TILEDB_PATH=/usr/local/'
@@ -19,7 +19,7 @@ export CIBW_BEFORE_TEST="pip install -r misc/requirements_wheel.txt"
 export CIBW_TEST_COMMAND="python -c 'import tiledb'"
 export TILEDB_WHEEL_BUILD=1
 
-docker build -t $CIBW_MANYLINUX_AARCH64_IMAGE -f misc/pypi_linux/Dockerfile.aarch64.manylinux2014 .
+docker build --build-arg=LIBTILEDB_VERSION=$LIBTILEDB_VERSION --build-arg TILEDBPY_VERSION=$TILEDBPY_VERSION -t $CIBW_MANYLINUX_AARCH64_IMAGE -f misc/pypi_linux/Dockerfile.aarch64.manylinux2014 .
 
 rm -rf /tmp/cibuildwheel_venv
 python3 -m venv /tmp/cibuildwheel_venv
