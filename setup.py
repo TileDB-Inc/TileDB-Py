7f30408a034a20f02cffa0d7de68c538b978da84
Vivian Nguyen <vivian@tiledb.com>
2022-02-02 20:23:03 -0600

    Release 0.12.2 Against 2.6.2 (#908)

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from pkg_resources import resource_filename
 from setuptools import Extension, find_packages, setup
 
 # Target branch
-TILEDB_VERSION = "2.6.1"
+TILEDB_VERSION = "2.6.2"
 # allow overriding w/ environment variable
 TILEDB_VERSION = os.environ.get("TILEDB_VERSION") or TILEDB_VERSION
 
