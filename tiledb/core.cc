d92a51766e331fb09bfbb769879e24cfb56eebfd
Isaiah Norton <isaiah@tiledb.io>
2022-04-28 22:39:14 -0400

    Arrow Buffer Ownership Handoff
    * This fixes an issue where `.query(return_arrow=True)` did not
      properly return an Arrow Table due to `~PyQuery` destroying the
      NumPy arrays holding the Arrow buffers upon returning from
      `multirange_indexing.DataFrameIndexer.__getitem__`. The buffers
      are now linked to the lifetime of the PyArrow Table rather
      than the `PyQuery` object
    * Move `arrow_io_impl.h` from core to `py_arrow_io_impl.h`
    * Addition of `BufferHolder` struct to hold `py::object`s `data`,
      `offset`, and `validity` which respectively hold the necessary
      buffers for the Arrow Table
    * In `PyQuery.pa_buffer`, create `BufferHolder` and pass buffers
      to increment refcount. Set `CPPArray.cbb_data` to `BufferHolder`
      pointer and `CPPSchema.release_cb` to `free_buffer_holder`.
      When the Arrow Table destructor is called, `free_buffer_holder`
      calls `delete` on the `BufferHolder` object which in turns
      decrements the refcount for the buffers

--- tiledb/core.cc
+++ tiledb/core.cc
@@ -18,10 +18,12 @@
 #define TILEDB_DEPRECATED
 #define TILEDB_DEPRECATED_EXPORT
 
-#include <tiledb/arrowio>
 #include <tiledb/tiledb.h>              // C
 #include <tiledb/tiledb>                // C++
 #include <tiledb/tiledb_experimental.h> // C
+
+#include "py_arrowio"
+#include "py_arrow_io_impl.h"
 
 #if defined(TILEDB_SERIALIZATION)
 #include <tiledb/tiledb_serialization.h> // C
@@ -118,6 +120,18 @@ struct BufferInfo {
   py::array data;
   py::array_t<uint64_t> offsets;
   py::array_t<uint8_t> validity;
+};
+
+struct BufferHolder {
+  py::object data;
+  py::object offsets;
+  py::object validity;
+
+  BufferHolder(py::object d, py::object o, py::object v) : data(d), offsets(o), validity(v) {}
+
+  static void free_buffer_holder(BufferHolder* self) {
+    delete self;
+  }
 };
 
 py::dtype tiledb_dtype(tiledb_datatype_t type, uint32_t cell_val_num) {
@@ -1349,7 +1363,7 @@ public:
     std::unique_ptr<PAPair> pa_pair(new PAPair());
 
     adapter.export_buffer(name.c_str(), &(pa_pair->array_),
-                          &(pa_pair->schema_));
+                          &(pa_pair->schema_), nullptr, nullptr);
     pa_pair->exported_ = true;
 
     return pa_pair;
@@ -1370,14 +1384,19 @@ public:
     py::list names;
     py::list results;
     for (auto &buffer_name : buffers_order_) {
+      BufferInfo &buffer_info = buffers_.at(buffer_name);
+
+      auto buffer_holder = new BufferHolder(buffer_info.data, buffer_info.validity, buffer_info.offsets);
+
       ArrowArray c_pa_array;
       ArrowSchema c_pa_schema;
       adapter.export_buffer(buffer_name.c_str(),
                             static_cast<void *>(&c_pa_array),
-                            static_cast<void *>(&c_pa_schema));
+                            static_cast<void *>(&c_pa_schema),
+                            (tiledb::arrow::ArrowAdapter::release_cb)BufferHolder::free_buffer_holder,
+                            buffer_holder);
 
       if (is_nullable(buffer_name)) {
-        BufferInfo &buffer_info = buffers_.at(buffer_name);
         // count zeros before converting to bitmap
         c_pa_array.null_count = count_zeros(buffer_info.validity);
         // convert to bitmap
