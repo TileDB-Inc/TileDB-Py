05b19ba9465131c7f9ceaf2fd92e8dbaf8306048
Vivian Nguyen <vivian@tiledb.com>
2022-08-25 11:19:31 -0500

    Remove Building 3.6 Wheels For Future Releases

--- misc/pypi_linux/build.sh
+++ misc/pypi_linux/build.sh
@@ -14,27 +14,13 @@ set -ex
 
 export TILEDB_PY_REPO="/opt/TileDB-Py"
 
-# build python36 wheel
-cd /home/tiledb
-git clone $TILEDB_PY_REPO TileDB-Py36
-git -C TileDB-Py36 checkout $TILEDBPY_VERSION
-
-cd /home/tiledb/TileDB-Py36
-/opt/python/cp36-cp36m/bin/python3.6 -m pip install -r misc/requirements_wheel.txt
-/opt/python/cp36-cp36m/bin/python3.6 setup.py build_ext bdist_wheel --tiledb=/usr/local
-auditwheel repair dist/*.whl
-/opt/python/cp36-cp36m/bin/python3.6 -m pip install wheelhouse/*.whl
-cd tiledb/tests
-#/opt/python/cp36-cp36m/bin/python3.6 -m unittest
-
-
 # build python37 wheel
 cd /home/tiledb
 git clone $TILEDB_PY_REPO TileDB-Py37
 git -C TileDB-Py37 checkout $TILEDBPY_VERSION
 
 cd /home/tiledb/TileDB-Py37
-/opt/python/cp36-cp36m/bin/python3.6 -m pip install -r misc/requirements_wheel.txt
+/opt/python/cp37-cp37m/bin/python3.7 -m pip install -r misc/requirements_wheel.txt
 /opt/python/cp37-cp37m/bin/python3.7 setup.py build_ext bdist_wheel --tiledb=/usr/local
 auditwheel repair dist/*.whl
 /opt/python/cp37-cp37m/bin/python3.7 -m pip install wheelhouse/*.whl
@@ -47,7 +33,7 @@ git clone $TILEDB_PY_REPO TileDB-Py38
 git -C TileDB-Py38 checkout $TILEDBPY_VERSION
 
 cd /home/tiledb/TileDB-Py38
-/opt/python/cp36-cp36m/bin/python3.6 -m pip install -r misc/requirements_wheel.txt
+/opt/python/cp38-cp38m/bin/python3.8 -m pip install -r misc/requirements_wheel.txt
 /opt/python/cp38-cp38/bin/python3.8 setup.py build_ext bdist_wheel --tiledb=/usr/local
 auditwheel repair dist/*.whl
 /opt/python/cp38-cp38/bin/python3.8 -m pip install wheelhouse/*.whl
@@ -59,7 +45,7 @@ git clone $TILEDB_PY_REPO TileDB-Py39
 git -C TileDB-Py39 checkout $TILEDBPY_VERSION
 
 cd /home/tiledb/TileDB-Py39
-/opt/python/cp36-cp36m/bin/python3.6 -m pip install -r misc/requirements_wheel.txt
+/opt/python/cp39-cp39m/bin/python3.9 -m pip install -r misc/requirements_wheel.txt
 /opt/python/cp39-cp39/bin/python3.9 setup.py build_ext bdist_wheel --tiledb=/usr/local
 auditwheel repair dist/*.whl
 /opt/python/cp39-cp39/bin/python3.9 -m pip install wheelhouse/*.whl
@@ -71,14 +57,14 @@ git clone $TILEDB_PY_REPO TileDB-Py310
 git -C TileDB-Py310 checkout $TILEDBPY_VERSION
 
 cd /home/tiledb/TileDB-Py310
-/opt/python/cp36-cp36m/bin/python3.6 -m pip install -r misc/requirements_wheel.txt
+/opt/python/cp310-cp310m/bin/python3.10 -m pip install -r misc/requirements_wheel.txt
 /opt/python/cp310-cp310/bin/python3.10 setup.py build_ext bdist_wheel --tiledb=/usr/local
 auditwheel repair dist/*.whl
 /opt/python/cp310-cp310/bin/python3.10 -m pip install wheelhouse/*.whl
 cd tiledb/tests
 
 # copy build products out
-cp /home/tiledb/TileDB-Py36/wheelhouse/* /wheels
 cp /home/tiledb/TileDB-Py37/wheelhouse/* /wheels
 cp /home/tiledb/TileDB-Py38/wheelhouse/* /wheels
 cp /home/tiledb/TileDB-Py39/wheelhouse/* /wheels
+cp /home/tiledb/TileDB-Py310/wheelhouse/* /wheels
