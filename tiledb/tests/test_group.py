3843b3a0bcdac00a02aaeee5c0fe72e43b55e598
Isaiah Norton <isaiah@tiledb.io>
2022-08-30 09:34:01 -0400

    Add delay in group tests to avoid timestamp overlap

--- tiledb/tests/test_group.py
+++ tiledb/tests/test_group.py
@@ -161,7 +161,11 @@ class GroupTest(GroupTestCase):
 
         grp.open("w")
         del grp.meta["int"]
+<<<<<<< HEAD
         time.sleep(0.001)
+=======
+        time.sleep(0.1)
+>>>>>>> Add delay in group tests to avoid timestamp overlap
         grp.close()
 
         grp = tiledb.Group(grp_path, "r")
