98b0b0cc9a7a0c90a65e9f0cc329144c3c902c3f
Vivian Nguyen <vivian@tiledb.com>
2022-11-10 17:21:11 -0600

    Test Release Against 2.13.0-rc1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from pybind11.setup_helpers import Pybind11Extension
 ### DO NOT USE ON CI
 
 # Target branch: Note that this should be set to the current core release, not `dev`
-TILEDB_VERSION = "2.12.2"
+TILEDB_VERSION = "2.13.0-rc1"
 
 # allow overriding w/ environment variable
 TILEDB_VERSION = os.environ.get("TILEDB_VERSION") or TILEDB_VERSION
