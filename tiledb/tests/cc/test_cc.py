3ed1f7cde248643d6d01f9ca8cc06b603603861f
Vivian Nguyen <vivian@tiledb.com>
2022-12-14 18:36:09 -0600

    Compare Normalize Paths Before Comparing

--- tiledb/tests/cc/test_cc.py
+++ tiledb/tests/cc/test_cc.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 import tiledb
 import tempfile
 
@@ -121,7 +122,7 @@ def test_array():
     ctx = lt.Context()
     arr = lt.Array(ctx, uri, lt.QueryType.READ)
     assert arr.is_open()
-    assert paths_equal(arr.uri(), uri)
+    assert paths_equal(os.path.normpath(arr.uri()), os.path.normpath(uri))
     assert arr.schema == arr.schema
 
     # TODO test
