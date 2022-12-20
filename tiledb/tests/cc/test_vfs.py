b5a4f16eceb6e217d2937176509ed2364104c0d4
Vivian Nguyen <vivian@tiledb.com>
2022-12-20 10:37:45 -0600

    Fix `cc` Unit Tests (#1519)

--- tiledb/tests/cc/test_vfs.py
+++ tiledb/tests/cc/test_vfs.py
@@ -1,14 +1,6 @@
-import numpy as np
-import tiledb
-import hypothesis
-import time
-import tempfile
 import os
 
 from tiledb import cc as lt
-from tiledb.tests.common import paths_equal
-
-import pytest
 
 
 def test_dir(tmp_path):
@@ -17,11 +9,11 @@ def test_dir(tmp_path):
 
     path = os.path.join(tmp_path, "test_dir")
 
-    vfs.create_dir(path)
-    assert vfs.is_dir(path) == True
-    assert vfs.dir_size(path) == 0
-    vfs.remove_dir(path)
-    assert vfs.is_dir(path) == False
+    vfs._create_dir(path)
+    assert vfs._is_dir(path) == True
+    assert vfs._dir_size(path) == 0
+    vfs._remove_dir(path)
+    assert vfs._is_dir(path) == False
 
 
 def test_file_handle(tmp_path):
@@ -31,15 +23,15 @@ def test_file_handle(tmp_path):
     path = os.path.join(tmp_path, "test_file_handle")
 
     fh = lt.FileHandle(ctx, vfs, path, lt.VFSMode.WRITE)
-    fh.write(b"Hello")
+    fh._write(b"Hello")
 
     fh = lt.FileHandle(ctx, vfs, path, lt.VFSMode.READ)
-    assert fh.read(0, 5) == b"Hello"
+    assert fh._read(0, 5) == b"Hello"
 
     fh = lt.FileHandle(ctx, vfs, path, lt.VFSMode.APPEND)
-    fh.write(b", world!")
+    fh._write(b", world!")
 
     fh = lt.FileHandle(ctx, vfs, path, lt.VFSMode.READ)
-    assert fh.read(0, 13) == b"Hello, world!"
+    assert fh._read(0, 13) == b"Hello, world!"
 
-    assert fh.closed == False
+    assert fh._closed == False
