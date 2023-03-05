7fc7915f5e5f828d0db4483df75245864adfec3e
Isaiah Norton <isaiah@tiledb.io>
2023-03-05 11:31:03 -0500

    WIP Bump version to 2.15.0-rc2

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import Extension, find_packages, setup
 ### DO NOT USE ON CI
 
 # Target branch: Note that this should be set to the current core release, not `dev`
-TILEDB_VERSION = "2.14.0"
+TILEDB_VERSION = "2.15.0-rc2"
 
 # allow overriding w/ environment variable
 TILEDB_VERSION = os.environ.get("TILEDB_VERSION") or TILEDB_VERSION
