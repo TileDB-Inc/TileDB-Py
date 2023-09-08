a5d418eb2148e448df7203a6df57c9386d8bdb21
Isaiah Norton <isaiah@tiledb.io>
2023-09-08 09:10:55 -0400

    Make pandas lazy import check conditional on tiledb.cloud version

--- tiledb/tests/test_basic_import.py
+++ tiledb/tests/test_basic_import.py
@@ -2,12 +2,32 @@ import ast
 import subprocess
 import sys
 
+import importlib_metadata
+from packaging.version import Version
+
 
 def test_dont_import_pandas() -> None:
     """Verifies that when we import TileDB, we don't import Pandas eagerly."""
+
+    # If tiledb.cloud < 0.10.21 is installed, we should remove it from the module list
+    # before running the test, because it eagerly imported pandas before that version.
+    # Note that we import tiledb.cloud within tiledb-py, if available, in order to hook
+    # Array.apply and other functionality.
+    try:
+        ver = importlib_metadata.version("tiledb.cloud")
+    except importlib_metadata.PackageNotFoundError:
+        ver = None
+    if ver and Version(ver) < Version("0.10.21"):
+        maybe_cloud = "sys.modules['tiledb.cloud'] = None;"
+    else:
+        maybe_cloud = ""
     # Get a list of all modules from a completely fresh interpreter.
     all_mods_str = subprocess.check_output(
-        (sys.executable, "-c", "import sys, tiledb; print(list(sys.modules))")
+        (
+            sys.executable,
+            "-c",
+            f"import sys; {maybe_cloud} import tiledb; print(list(sys.modules))",
+        )
     )
     all_mods = ast.literal_eval(all_mods_str.decode())
     assert "pandas" not in all_mods
