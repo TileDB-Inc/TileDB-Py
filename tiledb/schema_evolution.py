799a434332d1ef3d19ac977aaf477014dc2be114
Vivian Nguyen <vivian@tiledb.com>
2023-09-20 07:58:45 -0500

    Add Support For `SchemaEvolution` on Enumerations (#1834)
    This PR adds `SchemaEvolution.add_enumeration` and `SchemaEvolution.drop_enumeration`. The bindings were done in a previous PR but never added to `SchemaEvolution`.

--- tiledb/schema_evolution.py
+++ tiledb/schema_evolution.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 import tiledb
 
+from .enumeration import Enumeration
 from .main import ArraySchemaEvolution as ASE
 
 
@@ -28,6 +29,20 @@ class ArraySchemaEvolution:
         only applied when `ArraySchemaEvolution.array_evolve` is called."""
 
         self.ase.drop_attribute(attr_name)
+
+    def add_enumeration(self, enmr: Enumeration):
+        """Add the given enumeration to the schema evolution plan.
+        Note: this function does not apply any changes; the changes are
+        only applied when `ArraySchemaEvolution.array_evolve` is called."""
+
+        self.ase.add_enumeration(enmr)
+
+    def drop_enumeration(self, enmr_name: str):
+        """Drop the given enumeration (by name) in the schema evolution.
+        Note: this function does not apply any changes; the changes are
+        only applied when `ArraySchemaEvolution.array_evolve` is called."""
+
+        self.ase.drop_enumeration(enmr_name)
 
     def array_evolve(self, uri: str):
         """Apply ArraySchemaEvolution actions to Array at given URI."""
