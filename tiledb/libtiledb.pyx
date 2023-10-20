aca09d496ab9e6d0a3d55c29f0ea5b14f0f8aa97
John Kerl <kerl.john.r@gmail.com>
2023-10-20 14:29:07 -0400

    Allow delete mode for dense arrays

--- tiledb/libtiledb.pyx
+++ tiledb/libtiledb.pyx
@@ -2004,7 +2004,8 @@ cdef class DenseArrayImpl(Array):
         OrderedDict([('a1', array([0, 0, 0, 0, 0]))])
 
         """
-        if not self.isopen or self.mode != 'r':
+        #if not self.isopen or self.mode != 'r':
+        if not self.isopen or self.mode not in  ('r', 'd'):
             raise TileDBError("DenseArray must be opened in read mode")
 
         if attr_cond is not None:
@@ -2061,7 +2062,8 @@ cdef class DenseArrayImpl(Array):
         """
         from .subarray import Subarray
 
-        if not self.isopen or self.mode != 'r':
+        #if not self.isopen or self.mode != 'r':
+        if not self.isopen or self.mode not in  ('r', 'd'):
             raise TileDBError("DenseArray must be opened in read mode")
 
         if attr_cond is not None:
