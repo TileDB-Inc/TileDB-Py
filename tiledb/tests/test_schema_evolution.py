036bb439bee21becf478cf7e23bedec4f9c747f1
Vivian Nguyen <vivian@tiledb.com>
2023-10-25 14:18:34 -0500

    Format

--- tiledb/tests/test_schema_evolution.py
+++ tiledb/tests/test_schema_evolution.py
@@ -184,7 +184,7 @@ def test_schema_evolution_extend_enmr(tmp_path):
         assert A.schema.has_attr("a")
         assert A.attr("a").enum_label == "e"
         assert A.enum("e") == enmr
-        
+
     se = tiledb.ArraySchemaEvolution()
     with pytest.raises(tiledb.TileDBError):
         enmr.extend([1, 2, 3])
