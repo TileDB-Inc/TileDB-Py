56e21b39793096f59bfc7a4f332ff3568067075e
Julia Dark <24235303+jp-dark@users.noreply.github.com>
2023-10-25 10:56:07 -0400

    Add sparse dimension label example (#1843)
    * Add sparse dimension label example
    
    * Add tiles to dimension label schema
    
    Since this example will probably be copied, it's best to explicitly set
    the tile size to show how that is done.

--- examples/quickstart_dimension_labels_sparse.py
+++ examples/quickstart_dimension_labels_sparse.py
@@ -1,4 +1,4 @@
-# quickstart_dimension_labels.py
+# quickstart_dimension_labels_sparse.py
 #
 # LICENSE
 #
@@ -30,9 +30,8 @@
 #   https://docs.tiledb.com/main/how-to
 #   https://tiledb-inc-tiledb.readthedocs-hosted.com/projects/tiledb-py/en/stable/python-api.html
 #
-# When run, this program will create a simple 2D dense array with dimension labels, write
+# When run, this program will create a simple 2D sparse array with dimension labels, write
 # some data to it, and read a slice of the data back.
-#
 
 
 import numpy as np
@@ -42,54 +41,67 @@ import tiledb
 
 def create_array(uri: str):
     """Create array schema with dimension labels"""
-    dim1 = tiledb.Dim("d1", domain=(1, 5))
-    dim2 = tiledb.Dim("d2", domain=(1, 5))
+    dim1 = tiledb.Dim("index1", domain=(0, 5))
+    dim2 = tiledb.Dim("index2", domain=(0, 5))
     dom = tiledb.Domain(dim1, dim2)
-    att = tiledb.Attr("a1", dtype=np.int64)
+    att = tiledb.Attr("values", dtype=np.int64)
     dim_labels = {
-        0: {"l1": dim1.create_label_schema("decreasing", np.int64)},
-        1: {
-            "l2": dim2.create_label_schema("increasing", np.int64),
-            "l3": dim2.create_label_schema("increasing", np.float64),
-        },
+        0: {"x1": dim1.create_label_schema("increasing", np.float64, tile=6)},
+        1: {"x2": dim2.create_label_schema("increasing", np.float64, tile=6)},
     }
-    schema = tiledb.ArraySchema(domain=dom, attrs=(att,), dim_labels=dim_labels)
+    schema = tiledb.ArraySchema(
+        domain=dom, attrs=(att,), dim_labels=dim_labels, sparse=True
+    )
     tiledb.Array.create(uri, schema)
 
 
 def write_array(uri: str):
     """Write attribute and label data to the array"""
-    a1_data = np.reshape(np.arange(1, 26), (5, 5))
-    l1_data = np.arange(5, 0, -1)
-    l2_data = np.arange(-2, 3)
-    l3_data = np.linspace(-1.0, 1.0, 5)
+    # Use dimension label URIs to write directly to dimension label arrays.
+    schema = tiledb.ArraySchema.load(uri)
+    with tiledb.open(schema.dim_label("x1").uri, "w") as x1_array:
+        x1_array[:] = np.linspace(-1.0, 1.0, 6)
+    with tiledb.open(schema.dim_label("x2").uri, "w") as x2_array:
+        x2_array[:] = np.linspace(-1.0, 1.0, 6)
+
+    # Write a tridiagonal matrix.
+    coords1 = np.append(np.arange(1, 6), np.arange(5))
+    coords2 = np.append(np.arange(5), np.arange(1, 6))
+    data = np.append(np.arange(1, 6), -np.arange(1, 6))
     with tiledb.open(uri, "w") as array:
-        array[:] = {"a1": a1_data, "l1": l1_data, "l2": l2_data, "l3": l3_data}
+        array[coords1, coords2] = data
 
 
 def read_array(uri: str):
     """Read the array from the dimension label"""
 
     with tiledb.open(uri, "r") as array:
-        data1 = array.label_index(["l2"])[1, -1:1]
-        print("Reading array on [[1, -1:1]] with label 'l2' on dim2")
-        for name, value in data1.items():
-            print(f"  '{name}'={value}")
+        # Create a subarray and set label ranges.
+        subarray = tiledb.Subarray(array)
+        subarray.add_label_range("x1", (0.0, 1.0))
+        subarray.add_label_range("x2", (0.0, 1.0))
 
-        data2 = array.label_index(["l1", "l2"])[4:5, -2:2]
-        print("Reading array on [[4:5, -2:2]] with label 'l1' on dim1 and 'l2' on dim2")
-        for name, value in data2.items():
-            print(f"  '{name}'={value}")
+        # Get data from the main array.
+        data = array.read_subarray(subarray)
 
-        data3 = array.label_index(["l3"])[1, 0.0:2.0]
-        print("Reading array on [[1, 0.0:2.0]] with label 'l3' on dim2")
-        for name, value in data3.items():
+        # Read the labels.
+        x1_label = array.schema.dim_label("x1")
+        x2_label = array.schema.dim_label("x2")
+        with tiledb.open(x1_label.uri) as x1_array:
+            x1_coords = x1_array.multi_index[data["index1"]]
+            data["x1"] = x1_coords[x1_label.label_attr_name]
+        with tiledb.open(array.schema.dim_label("x2").uri) as x2_array:
+            x2_coords = x2_array.multi_index[data["index2"]]
+            data["x2"] = x2_coords[x2_label.label_attr_name]
+
+        print("Reading data where 0.0 <= x1 <= 2.0 and -2.0 <= x2 <= 0.0")
+        for name, value in data.items():
             print(f"  '{name}'={value}")
 
 
 if __name__ == "__main__":
     # Name of the array to create.
-    ARRAY_NAME = "quickstart_labels"
+    ARRAY_NAME = "quickstart_labels_sparse"
 
     LIBVERSION = tiledb.libtiledb.version()
 
