9d21277db129a0df3c1a064a2517fd1797574f7a
Vivian Nguyen <vivian@tiledb.com>
2024-01-18 16:29:01 -0600

    Safe repr for `Group`

--- HISTORY.md
+++ HISTORY.md
@@ -1,5 +1,9 @@
 # Next
 
+## Bug Fixes
+
+* Add safe `Group.__repr__` []()
+
 ## Improvements
 
 * Warn when `os.fork()` is used in the presence of a Tiledb context [#1876](https://github.com/TileDB-Inc/TileDB-Py/pull/1876/files).
--- tiledb/tests/test_group.py
+++ tiledb/tests/test_group.py
@@ -341,6 +341,11 @@ class GroupTest(GroupTestCase):
 
             with tiledb.Group(group_uri, config=cfg) as G:
                 assert len(G) == sz
+    
+    def test_group_does_not_exist(self):
+        with self.assertRaises(tiledb.TileDBError):
+            tiledb.Group("does-not-exist")
+
 
 
 class GroupMetadataTest(GroupTestCase):
@@ -616,3 +621,4 @@ class GroupMetadataTest(GroupTestCase):
         tiledb.Group.vacuum_metadata(path)
 
         assert len(vfs.ls(meta_path)) == 1
+    
