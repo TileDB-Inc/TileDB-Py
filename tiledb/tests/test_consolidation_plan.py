401bf93b2a16e9f27cd72bd0a88954bc8b472674
Agis Kounelis <kounelisagis@gmail.com>
2024-03-26 15:05:56 +0200

    change variable name

--- tiledb/tests/test_consolidation_plan.py
+++ tiledb/tests/test_consolidation_plan.py
@@ -14,27 +14,27 @@ class ConsolidationPlanTest(DiskTestCase):
         tiledb.from_numpy(path, array)
 
         with tiledb.open(path, "r") as A:
-            xxx = tiledb.ConsolidationPlan(tiledb.default_ctx(), A, 2)
-            assert xxx.num_nodes == 1
-            assert xxx.num_fragments(0) == 1
+            cons_plan = tiledb.ConsolidationPlan(tiledb.default_ctx(), A, 2)
+            assert cons_plan.num_nodes == 1
+            assert cons_plan.num_fragments(0) == 1
             # check that it has a nodes key
-            assert "nodes" in json.loads(xxx.dump())
+            assert "nodes" in json.loads(cons_plan.dump())
             # check that it has a list of nodes
-            assert isinstance(json.loads(xxx.dump())["nodes"], list)
+            assert isinstance(json.loads(cons_plan.dump())["nodes"], list)
             # check that each node has a uri key
-            assert "uri" in json.loads(xxx.dump())["nodes"][0]["uris"][0]
+            assert "uri" in json.loads(cons_plan.dump())["nodes"][0]["uris"][0]
 
         # write a second fragment to the array
         with tiledb.open(path, "w") as A:
             A[:] = np.random.rand(4)
 
         with tiledb.open(path, "r") as A:
-            xxx = tiledb.ConsolidationPlan(tiledb.default_ctx(), A, 4)
-            assert xxx.num_nodes == 1
-            assert xxx.num_fragments(0) == 2
+            cons_plan = tiledb.ConsolidationPlan(tiledb.default_ctx(), A, 4)
+            assert cons_plan.num_nodes == 1
+            assert cons_plan.num_fragments(0) == 2
             # check that it has a nodes key
-            assert "nodes" in json.loads(xxx.dump())
+            assert "nodes" in json.loads(cons_plan.dump())
             # check that it has a list of nodes
-            assert isinstance(json.loads(xxx.dump())["nodes"], list)
+            assert isinstance(json.loads(cons_plan.dump())["nodes"], list)
             # check that each node has a uri key
-            assert "uri" in json.loads(xxx.dump())["nodes"][0]["uris"][0]
+            assert "uri" in json.loads(cons_plan.dump())["nodes"][0]["uris"][0]
