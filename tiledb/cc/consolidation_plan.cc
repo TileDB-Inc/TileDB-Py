e874bbde86dde78c62584c040a3ca04fc2634fe6
kounelisagis <kounelisagis@gmail.com>
2024-03-26 12:46:44 +0000

    Fix clang-formatting

--- tiledb/cc/consolidation_plan.cc
+++ tiledb/cc/consolidation_plan.cc
@@ -17,11 +17,12 @@ namespace py = pybind11;
 void init_consolidation_plan(py::module &m) {
   py::class_<ConsolidationPlan>(m, "ConsolidationPlan")
 
-      .def(py::init<Context &, Array &, uint64_t>(), py::keep_alive<1, 2>(), py::keep_alive<1, 3>())
+      .def(py::init<Context &, Array &, uint64_t>(), py::keep_alive<1, 2>(),
+           py::keep_alive<1, 3>())
 
       .def_property_readonly("_num_nodes", &ConsolidationPlan::num_nodes)
       .def("_num_fragments", &ConsolidationPlan::num_fragments)
       .def("_fragment_uri", &ConsolidationPlan::fragment_uri)
       .def("_dump", &ConsolidationPlan::dump);
-  }
 }
+} // namespace libtiledbcpp
