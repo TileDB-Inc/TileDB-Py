47f116193a4887d2581e4b8788fc3a8ed1e13c8e
Agis Kounelis <kounelisagis@gmail.com>
2024-07-26 20:24:59 +0300

    Remove types from docstring

--- tiledb/array_schema.py
+++ tiledb/array_schema.py
@@ -38,18 +38,12 @@ class ArraySchema(CtxMixin, lt.ArraySchema):
     Schema class for TileDB dense / sparse array representations
 
     :param domain: Domain of schema
-    :type domain: tiledb.Domain
     :param attrs: tuple of attributes
-    :type attrs: tuple(tiledb.Attr, ...)
     :param cell_order:  TileDB label for cell layout
-    :type cell_order: 'row-major' (default) or 'C', 'col-major' or 'F' or 'hilbert'
     :param tile_order:  TileDB label for tile layout
-    :type tile_order: 'row-major' (default) or 'C', 'col-major' or 'F'
     :param int capacity: tile cell capacity
     :param offsets_filters: (default None) offsets filter list
-    :type offsets_filters: tiledb.FilterList
     :param validity_filters: (default None) validity filter list
-    :type validity_filters: tiledb.FilterList
     :param bool allows_duplicates: True if duplicates are allowed
     :param bool sparse: True if schema is sparse, else False \
         (set by SparseArray and DenseArray derived classes)
