3fbeffec57e7b8a354b1588d3c0a90d78a0df313
Agisilaos Kounelis <kounelisagis@gmail.com>
2024-08-10 01:08:56 +0300

    Avoid libtiledb_version function exposure to the public API

--- tiledb/__init__.py
+++ tiledb/__init__.py
@@ -2,8 +2,6 @@ import ctypes
 import os
 import sys
 
-from tiledb.libtiledb import version as libtiledb_version
-
 # un-comment this section to fix Cython backtrace line-numbers in
 # IPython/Jupyter. see https://bugs.python.org/issue32797#msg323167
 # ---
@@ -21,14 +19,19 @@ if os.name == "posix":
 else:
     lib_name = "tiledb"
 
+from tiledb.libtiledb import version as libtiledb_version
+
+if libtiledb_version()[0] == 2 and libtiledb_version()[1] >= 25:
+    from .current_domain import CurrentDomain
+    from .ndrectangle import NDRectangle
+
+del libtiledb_version  # no longer needed
+
 from .array_schema import ArraySchema
 from .attribute import Attr
 from .cc import TileDBError
 from .consolidation_plan import ConsolidationPlan
 from .ctx import Config, Ctx, default_ctx, scope_ctx
-
-if libtiledb_version()[0] == 2 and libtiledb_version()[1] >= 25:
-    from .current_domain import CurrentDomain
 from .dataframe_ import from_csv, from_pandas, open_dataframe
 from .dimension import Dim
 from .dimension_label import DimLabel
@@ -95,9 +98,6 @@ from .libtiledb import (
     walk,
 )
 from .multirange_indexing import EmptyRange
-
-if libtiledb_version()[0] == 2 and libtiledb_version()[1] >= 25:
-    from .ndrectangle import NDRectangle
 from .object import Object
 from .parquet_ import from_parquet
 from .query import Query
