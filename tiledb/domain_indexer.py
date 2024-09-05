02affdac5e122644aeb5f286ea685559df4b4e01
Agis Kounelis <kounelisagis@gmail.com>
2024-09-05 18:25:09 +0000

    Linting

--- tiledb/domain_indexer.py
+++ tiledb/domain_indexer.py
@@ -2,8 +2,8 @@ import weakref
 
 import numpy as np
 
-import tiledb.cc as lt
 import tiledb
+import tiledb.cc as lt
 
 
 def _index_as_tuple(idx):
@@ -12,8 +12,8 @@ def _index_as_tuple(idx):
         return idx
     return (idx,)
 
-class DomainIndexer:
 
+class DomainIndexer:
     @staticmethod
     def with_schema(schema):
         indexer = DomainIndexer.__new__(DomainIndexer)
@@ -21,7 +21,7 @@ class DomainIndexer:
         indexer.schema = schema
         return indexer
 
-    def __init__(self, array, query = None):
+    def __init__(self, array, query=None):
         self.array_ref = weakref.ref(array)
         self.schema = array.schema
         self.query = query
@@ -29,16 +29,17 @@ class DomainIndexer:
     # @property
     def schema(self):
         return self.array.array_ref().schema
-    
 
     @property
     def array(self):
-        assert self.array_ref() is not None, \
-            "Internal error: invariant violation (index[] with dead array_ref)"
+        assert (
+            self.array_ref() is not None
+        ), "Internal error: invariant violation (index[] with dead array_ref)"
         return self.array_ref()
 
     def __getitem__(self, idx):
-        from .subarray import Subarray # prevent circular import
+        from .subarray import Subarray  # prevent circular import
+
         # implements domain-based indexing: slice by domain coordinates, not 0-based python indexing
 
         schema = self.array.schema
@@ -49,8 +50,10 @@ class DomainIndexer:
         idx = _index_as_tuple(idx)
 
         if len(idx) < dom.ndim:
-            raise IndexError("number of indices does not match domain rank: "
-                             "(got {!r}, expected: {!r})".format(len(idx), ndim))
+            raise IndexError(
+                "number of indices does not match domain rank: "
+                "(got {!r}, expected: {!r})".format(len(idx), ndim)
+            )
 
         new_idx = []
         for i in range(dom.ndim):
@@ -83,27 +86,37 @@ class DomainIndexer:
         if self.query is not None:
             # if we are called via Query object, then we need to respect Query semantics
             order = self.query.order
-            attr_names = self.query.attrs if self.query.attrs else attr_names # query.attrs might be None -> all
+            attr_names = (
+                self.query.attrs if self.query.attrs else attr_names
+            )  # query.attrs might be None -> all
             attr_cond = self.query.attr_cond
             coords = self.query.coords
 
         if coords:
-            attr_names = [dom.dim(idx).name for idx in range(self.schema.ndim)] + attr_names
+            attr_names = [
+                dom.dim(idx).name for idx in range(self.schema.ndim)
+            ] + attr_names
 
-        if order is None or order == 'C':
+        if order is None or order == "C":
             layout = lt.LayoutType.ROW_MAJOR
-        elif order == 'F':
+        elif order == "F":
             layout = lt.LayoutType.COL_MAJOR
-        elif order == 'G':
+        elif order == "G":
             layout = lt.LayoutType.GLOBAL_ORDER
-        elif order == 'U':
+        elif order == "U":
             layout = lt.LayoutType.UNORDERED
         else:
-            raise ValueError("order must be 'C' (TILEDB_ROW_MAJOR), 'F' (TILEDB_COL_MAJOR), or 'G' (TILEDB_GLOBAL_ORDER)")
+            raise ValueError(
+                "order must be 'C' (TILEDB_ROW_MAJOR), 'F' (TILEDB_COL_MAJOR), or 'G' (TILEDB_GLOBAL_ORDER)"
+            )
 
         if isinstance(self.array, tiledb.libtiledb.SparseArrayImpl):
-            return self.array._read_sparse_subarray(subarray, attr_names, attr_cond, layout)
+            return self.array._read_sparse_subarray(
+                subarray, attr_names, attr_cond, layout
+            )
         elif isinstance(self.array, tiledb.libtiledb.DenseArrayImpl):
-            return self.array._read_dense_subarray(subarray, attr_names, attr_cond, layout, coords)
+            return self.array._read_dense_subarray(
+                subarray, attr_names, attr_cond, layout, coords
+            )
         else:
             raise Exception("No handler for Array type: " + str(type(self.array)))
