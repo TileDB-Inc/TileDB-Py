8e2b5cb676105079bb15862cfea80736c487bd6a
Agisilaos Kounelis <kounelisagis@gmail.com>
2024-10-12 00:41:05 +0300

    Update the error message that appears in test

--- tiledb/tests/test_enumeration.py
+++ tiledb/tests/test_enumeration.py
@@ -93,7 +93,7 @@ class EnumerationTest(DiskTestCase):
             with self.assertRaises(tiledb.TileDBError) as excinfo:
                 assert A.enum("enmr3") == []
             assert (
-                "ArraySchema: Unable to check if unknown enumeration is loaded. No enumeration named 'enmr3'."
+                "Array: Unable to get enumeration; Enumeration 'enmr3' does not exist."
                 == str(excinfo.value)
             )
             assert attr3.enum_label is None
