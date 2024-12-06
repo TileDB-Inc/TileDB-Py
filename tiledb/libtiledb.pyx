e8b2b9872d400c3d40e2cc9a0beb9d4565854783
Agisilaos Kounelis <kounelisagis@gmail.com>
2024-12-06 21:02:45 +0200

    Delete libtiledb.pxd and helper functions

--- tiledb/libtiledb.pyx
+++ tiledb/libtiledb.pyx
@@ -24,39 +24,3 @@ np.import_array()
 
 # Use unified numpy printing
 np.set_printoptions(legacy="1.21" if np.lib.NumpyVersion(np.__version__) >= "1.22.0" else False)
-
-
-cdef tiledb_ctx_t* safe_ctx_ptr(object ctx):
-    if ctx is None:
-        raise TileDBError("internal error: invalid Ctx object")
-    return <tiledb_ctx_t*>PyCapsule_GetPointer(ctx.__capsule__(), "ctx")
-
-cdef _raise_tiledb_error(tiledb_error_t* err_ptr):
-    cdef const char* err_msg_ptr = NULL
-    ret = tiledb_error_message(err_ptr, &err_msg_ptr)
-    if ret != TILEDB_OK:
-        tiledb_error_free(&err_ptr)
-        if ret == TILEDB_OOM:
-            raise MemoryError()
-        raise TileDBError("error retrieving error message")
-    cdef unicode message_string
-    try:
-        message_string = err_msg_ptr.decode('UTF-8', 'strict')
-    finally:
-        tiledb_error_free(&err_ptr)
-    raise TileDBError(message_string)
-
-
-cdef _raise_ctx_err(tiledb_ctx_t* ctx_ptr, int rc):
-    if rc == TILEDB_OK:
-        return
-    if rc == TILEDB_OOM:
-        raise MemoryError()
-    cdef tiledb_error_t* err_ptr = NULL
-    cdef int ret = tiledb_ctx_get_last_error(ctx_ptr, &err_ptr)
-    if ret != TILEDB_OK:
-        tiledb_error_free(&err_ptr)
-        if ret == TILEDB_OOM:
-            raise MemoryError()
-        raise TileDBError("error retrieving error object from ctx")
-    _raise_tiledb_error(err_ptr)
