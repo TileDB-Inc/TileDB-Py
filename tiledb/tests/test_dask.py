3bafb00bb31d7048d7ffda84e2479ecbd8a7d808
Agisilaos Kounelis <36283973+kounelisagis@users.noreply.github.com>
2025-02-14 23:18:42 +0200

    Fix failing tests (#2162)
    * Fix current domain test
    * Fix dask test

--- tiledb/tests/test_dask.py
+++ tiledb/tests/test_dask.py
@@ -1,11 +1,7 @@
-import sys
 import warnings
-from datetime import datetime
 
 import numpy as np
 import pytest
-from distributed.comm.core import CommClosedError
-from tornado.iostream import StreamClosedError
 
 import tiledb
 
@@ -44,9 +40,7 @@ class TestDaskSupport(DiskTestCase):
 
         tiledb.DenseArray.create(uri, schema)
 
-    @pytest.mark.flaky(
-        reruns=3, reruns_delay=2, only_rerun=(CommClosedError, StreamClosedError)
-    )
+    @pytest.mark.flaky(reruns=3, reruns_delay=2, rerun_except="TileDBError")
     @pytest.mark.filterwarnings("ignore:There is no current event loop")
     def test_dask_multiattr_2d(self):
         uri = self.path("multiattr")
