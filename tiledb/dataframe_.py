6d2cb59cfff2ea3a380e664553dc58ac0948b901
John Kerl <kerl.john.r@gmail.com>
2025-06-30 06:01:02 -0400

    Fix stacked sparse CSV ingestion (#2208)

--- tiledb/dataframe_.py
+++ tiledb/dataframe_.py
@@ -739,12 +739,15 @@ def _write_array(
                     and dim_name not in df.index.names
                     and dim_name != "__tiledb_rows"
                 ):
-                    # this branch handles the situation where a user did not specify
+                    # This branch handles the situation where a user did not specify
                     # index_col and is using mode='append'. We would like to try writing
                     # with the columns corresponding to existing dimension name.
-                    coords.append(write_dict.pop(dim_name))
+                    coord = write_dict.pop(dim_name)
                 else:
-                    coords.append(df.index.get_level_values(k))
+                    coord = df.index.get_level_values(k)
+                if row_start_idx is not None and dim_name == "__tiledb_rows":
+                    coord += row_start_idx
+                coords.append(coord)
             # TODO ensure correct col/dim ordering
             tiledb.sparse_array._setitem_impl_sparse(
                 A, tuple(coords), write_dict, nullmaps
