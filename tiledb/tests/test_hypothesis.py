f56d3a07b5a20a92be2e96ba9f4664c00e144c83
Agisilaos Kounelis <kounelisagis@gmail.com>
2026-09-25 12:03:31 +0300

    Drop the wall-clock limit from the bytes round-trip test
    The test failed whenever its body took over 2s, which a shared CI runner does now and then, while the round trip itself passed. It was left in to debug #1194; the test now checks only what it reads back.

--- tiledb/tests/test_hypothesis.py
+++ tiledb/tests/test_hypothesis.py
@@ -1,5 +1,3 @@
-import time
-
 import hypothesis as hp
 import hypothesis.strategies as st
 import numpy as np
@@ -18,14 +16,9 @@ tm = pd._testing
 @hp.settings(deadline=None, verbosity=hp.Verbosity.verbose)
 @hp.given(st.binary())
 def test_bytes_npdf(checked_path, mode, data):
-    start = time.time()
-
     uri = "mem://" + checked_path.path()
-    hp.note(f"!!! path '{uri}' time: {time.time() - start}")
-
     array = np.array([data], dtype="S0")
 
-    start_ingest = time.time()
     if mode == "np":
         with tiledb.from_numpy(uri, array) as A:
             pass
@@ -35,28 +28,8 @@ def test_bytes_npdf(checked_path, mode, data):
         # NOTE: ctx required here for mem://
         tiledb.from_pandas(uri, df, sparse=False, ctx=tiledb.default_ctx())
 
-    hp.note(f"{mode} ingest time: {time.time() - start_ingest}")
-
-    # DEBUG
-    tiledb.stats_enable()
-    tiledb.stats_reset()
-    # END DEBUG
-
     with tiledb.open(uri) as A:
         if mode == "np":
             np.testing.assert_array_equal(A.multi_index[:][""], array)
         else:
             tm.assert_frame_equal(A.df[:], df)
-
-    hp.note(tiledb.stats_dump(print_out=False))
-
-    # DEBUG
-    tiledb.stats_disable()
-
-    duration = time.time() - start
-    hp.note(f"!!! test_bytes_{mode} duration: {duration}")
-    if duration > 2:
-        # Hypothesis setup is (maybe) causing deadline exceeded errors
-        # https://github.com/TileDB-Inc/TileDB-Py/issues/1194
-        # Set deadline=None and use internal timing instead.
-        pytest.fail(f"!!! {mode} function body duration exceeded 2s: {duration}")
